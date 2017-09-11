a2695ac7a8f3d3ac9796ac650b98c9744bbe2785
Daniel Vaca Araujo <Diviei@users.noreply.github.com>
2017-09-11 10:35:34 +0200

    angular assets are not compressed on prod (#8)

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ mkdir compiled
 yuglify assets/css/*.css -c compiled/styles
 yuglify app/*.js app/**/*.js app/**/**/*.js -c compiled/app
 yuglify assets/js/codemirror.js assets/js/codemirror_yara.js assets/js/moment-with-locales.js assets/js/qrcode.min.js -c compiled/assets
-yuglify assets/js/mentio.min.js assets/js/angular*.js assets/js/ui*.js -c compiled/angular-assets
+# yuglify assets/js/mentio.min.js assets/js/angular*.js assets/js/ui*.js -c compiled/angular-assets
 rm index.html
 cp index_prod.html index.html
